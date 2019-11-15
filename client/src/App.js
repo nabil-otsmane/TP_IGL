d5248bda77c50deb7593ad0b43b99fe538bb74e9
nabil-otsmane <hn_otsmane@esi.dz>
2019-11-15 21:43:52 +0100

    fix: added dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React ,{Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'react-bootstrap';
 
 class App extends Component {

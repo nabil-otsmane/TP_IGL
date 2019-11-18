0677eaa41202e954ab6c95744bcbbc5b4fe9a603
Raiah Mohamed Amine <hm_raiah@esi.dz>
2019-11-18 03:59:36 +0100

    dev: Add Etudiant's form and work perfectly *-*

--- client/src/mainPage/table.js
+++ client/src/mainPage/table.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilter, faTag, faSortAlphaDownAlt, faBook, faLock, faCalendar, faEnvelopeOpenText, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { faFilter, faTag, faSortAlphaDownAlt, faBook, faLock, faCalendar, faEnvelopeOpenText, faNewspaper, faAddressBook } from '@fortawesome/free-solid-svg-icons';
 import { OverlayTrigger, Popover } from 'react-bootstrap';
 
 class Table extends Component {
@@ -145,7 +145,7 @@ class Table extends Component {
 
           const win = this.props.type;
 
-          const poppe = (
+          const popEnseignant = (
             <Popover id="popOver" >
                       <Popover.Title as="h1">
                           Please Fill this Form
@@ -206,10 +206,70 @@ class Table extends Component {
                             </form>
                       </Popover.Content>
           </Popover>   );
+
+
+          const popEtudiant = (
+            <Popover id="popOver" >
+                      <Popover.Title as="h1">
+                          Please Fill this Form
+                      </Popover.Title>
+                      <Popover.Content>
+                            <form style={{margin:"10px"}}>
+
+                                <div className="wrap-input100 validate-input" data-validate = "Valid Family Name is required" >
+                                  <input className="input100" type="text" name="familyNameStudent" placeholder="Family Name"/>
+                                  <span className="focus-input100"/>
+                                  <span className="symbol-input100">
+                                      <FontAwesomeIcon icon={faNewspaper} color="#1d2a48" size="sm" />   
+                                    </span>
+                              </div>
+
+                                <div className="wrap-input100 validate-input" data-validate = "Valid first Na1me is required" >
+                                    <input className="input100" type="text" name="firstNameStudent" placeholder="First Name"/>
+                                    <span className="focus-input100"/>
+                                    <span className="symbol-input100">
+                                      <FontAwesomeIcon icon={faEnvelopeOpenText} color="#1d2a48" size="sm" />   
+                                      </span>
+                                </div> 
+
+                                <div className="wrap-input100 validate-input"  >
+                                    <input className="input100" type="date" name="dateNaissanceStudent" placeholder="Date de Naissance"/>
+                                    <span className="focus-input100"/>
+                                    <span className="symbol-input100">
+                                      <FontAwesomeIcon icon={faCalendar} color="#1d2a48" size="sm" />   
+                                      </span>
+                                </div> 
+
+                                <div className="wrap-input100 validate-input"  >
+                                    <input className="input100" type="text" name="NiveauStudent" placeholder="X C(P/S) Y"/>
+                                    <span className="focus-input100"/>
+                                    <span className="symbol-input100">
+                                      <FontAwesomeIcon icon={faAddressBook} color="#1d2a48" size="sm" />   
+                                      </span>
+                                </div> 
+
+                                <div className="wrap-input100 validate-input"  >
+                                    <input className="input100" type="tel" name="matStudent" placeholder="Matricule"/>
+                                    <span className="focus-input100"/>
+                                    <span className="symbol-input100">
+                                      <FontAwesomeIcon icon={faBook} color="#1d2a48" size="sm" />   
+                                      </span>
+                                </div> 
+            
+                                <button className="login100-form-btn" onClick ={
+                                  console.log("Validate")
+                                }>
+                                  Validate
+                                </button>
+
+                            </form>
+                      </Popover.Content>
+          </Popover>   );          
         return (
             <div className="mt-4 bg-light w-100">
               <div className="d-flex bg-light" style={{height: 60}}>
-                <OverlayTrigger trigger="click" placement="right" overlay={poppe}>
+                <OverlayTrigger trigger="click" placement="right" overlay={
+                  win.toUpperCase().slice(0, win.length-1) ==="ENSEIGNANT" ? popEnseignant : popEtudiant}>
                 <button className="btn m-3 p-0 pl-2 pr-2 float-left btn-voilet">
                     <small>ADD {win.toUpperCase().slice(0, win.length-1)}</small>
                 </button>

b563f3cc8a6e00453901951b48e397a3f3c4727e
Raiah Mohamed Amine <hm_raiah@esi.dz>
2019-11-26 14:52:27 +0100

    dev: Geeting Form's inputs and verifying with regex

--- client/src/mainPage/table.js
+++ client/src/mainPage/table.js
@@ -1,14 +1,267 @@
 import React, { Component } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFilter, faTag, faSortAlphaDownAlt } from '@fortawesome/free-solid-svg-icons';
-import { OverlayTrigger } from 'react-bootstrap';
-
-import {popEnseignant, popEtudiant} from './popUps';
+import { faFilter, faTag, faSortAlphaDownAlt,faBook, faLock, faCalendar, faEnvelopeOpenText, faNewspaper, faAddressBook } from '@fortawesome/free-solid-svg-icons';
+import { OverlayTrigger, Alert } from 'react-bootstrap';
+import { Popover } from 'react-bootstrap';
 
 /** The main Page where the admin can see and add Ensgants and Etudiants*/
 class Table extends Component {
+
+    constructor(props)
+    {
+      super(props);
+      this.props=props;
+      this.ValidateProfClicked = this.ValidateProfClicked.bind(this);
+      this.ValidateEtudiantClicked= this.ValidateEtudiantClicked.bind(this);
+    }
+
+    ValidateProfClicked(s)
+    {
+        var {FamilyNameProf, FirstNameProf,AgeProf,NSSProf} =this.refs;
+        if (/[A-Za-z]{3,20}/.test(FamilyNameProf.value)
+             &&  /[A-Za-z]{1,25}[A-Za-z]{1,25}?/.test(FirstNameProf.value) 
+             &&  /[0-9]{2}/.test(AgeProf.value) && AgeProf.value >=20 && AgeProf.value<=50
+             &&  /[1-9][0-9]{12}/.test(NSSProf.value) 
+             )  console.log("ACCEPted");
+        else
+        {
+            console.log("not Accepted");
+        }     
+        
+    }
+
+    ValidateEtudiantClicked ()
+    {
+
+        var {FamilyNameEtudiant,FirstNameEtudiant,DateNaissanceEtudiant,NiveauEtudiant,MatEtudiant}= this.refs;
+        if(
+            /[A-Za-z]{3,20}/.test(FamilyNameEtudiant.value)
+            && /[A-Za-z]{1,25}[A-Za-z]{1,25}?/.test(FirstNameEtudiant.value)
+            && //Date de naiussance 
+            /[1-3]CP?S?\d/.test(NiveauEtudiant.value) 
+            && /[0-9]{13}/.test(MatEtudiant.value)
+        )console.log("Accepted");
+        
+        else{
+            console.log(DateNaissanceEtudiant.value);
+        }
+
+    }
+
     render() {
+
+            const popEnseignant = (
+                <Popover id = "popOver" >    
+            <Popover.Title as="h1" >
+                Please Fill this Form 
+            </Popover.Title> 
+            <Popover.Content >
+                <form style = {
+                    { margin: "10px" } } >
+    
+                    <div className = "wrap-input100 validate-input" >
+                        <input className = "input100"   
+                            ref="FamilyNameProf"
+                            type = "text"
+                            name = "familyNameProf"
+                            placeholder = "Family Name"
+                            minLength = "3"
+                            maxLength = "20"
+                            pattern = "[A-Za-z]{3,20}"
+                            title = "Should contain at least 3 letters "
+                            required />
+                        <span className = "focus-input100" />
+                        <span className = "symbol-input100" >
+                            <FontAwesomeIcon icon = { faNewspaper }
+                            color = "#1d2a48"
+                            size = "sm" />
+                        </span> 
+                    </div>
+    
+                    <div className = "wrap-input100 validate-input" >
+                        <input className = "input100"
+                            ref="FirstNameProf"
+                            type = "text"
+                            name = "firstNameProf"
+                            placeholder = "First Name"
+                            maxLength = "25"
+                            pattern = "[A-Za-z]{1,25}[A-Za-z]{1,25}?"
+                            title = "Accept Only Letters "
+                            required />
+                        <span className = "focus-input100" />
+                        <span className = "symbol-input100" >
+                            <FontAwesomeIcon icon = { faEnvelopeOpenText }
+                            color = "#1d2a48"
+                            size = "sm" />
+                        </span> 
+                    </div> 
+    
+                    <div className = "wrap-input100 validate-input" >
+                        <input className = "input100"
+                            ref="AgeProf"
+                            type = "number"
+                            min = "20"
+                            max = "50"
+                            name = "ageProf"
+                            placeholder = "Age"
+                            title = "The age must be between 20 and 50"
+                            required />
+                        <span className = "focus-input100" />
+                        <span className = "symbol-input100" >
+                            <FontAwesomeIcon icon = { faCalendar }
+                            color = "#1d2a48"
+                            size = "sm" />
+                        </span> 
+                    </div> 
+    
+                    <div className = "wrap-input100 validate-input" >
+                        <input className = "input100"
+                            ref="NSSProf"
+                            type = "tel"
+                            name = "NSSProf"
+                            placeholder = "Social Security Number"
+                            minLength = "13"
+                            maxLength = "13"
+                            pattern = "[1-9][0-9]{12}"
+                            title = "Your Social Security Number dont start with 0 and must contain 13 Digit"
+                            required />
+                        <span className = "focus-input100" />
+                        <span className = "symbol-input100" >
+                            <FontAwesomeIcon icon = { faLock }
+                            color = "#1d2a48"
+                            size = "sm" />
+                        </span> 
+                    </div> 
+    
+                    <div className = "wrap-input100 validate-input" >
+                        <select className = "input100" >
+                            <option > Algorithmique </option> 
+                            <option > Mathematique </option> 
+                            <option > Autre </option> 
+                        </select> 
+                        <span className = "focus-input100" />
+                        <span className = "symbol-input100" >
+                            <FontAwesomeIcon icon = { faBook }
+                                color = "#1d2a48"
+                                size = "sm" />
+                        </span> 
+                    </div> 
+    
+                    <button className = "login100-form-btn"
+                            onClick ={ this.ValidateProfClicked} >
+                        Validate 
+                    </button>
+                </form> 
+            </Popover.Content> 
+        </Popover>
+            );
+
+            const popEtudiant = (
+                <Popover id = "popOver" >
+            <Popover.Title as = "h1" >
+                Please Fill this Form 
+            </Popover.Title> 
+            <Popover.Content >
+                <form style = {
+                        { margin: "10px" } } >
+        
+                    <div className = "wrap-input100 validate-input" >
+                    <input className = "input100"
+                    ref="FamilyNameEtudiant"
+                    type = "text"
+                    name = "familyNameStudent"
+                    placeholder = "Family Name"
+                    minLength = "3"
+                    maxLength = "20"
+                    pattern = "[A-Za-z]{3,20}"
+                    title = "Should contain at least 3 letters "
+                    required />
+                    <span className = "focus-input100" />
+                    <span className = "symbol-input100" >
+                    <FontAwesomeIcon icon = { faNewspaper }
+                    color = "#1d2a48"
+                    size = "sm" />
+                    </span> 
+                    </div>
+        
+                    <div className = "wrap-input100 validate-input" >
+                    <input className = "input100"
+                    ref="FirstNameEtudiant"
+                    type = "text"
+                    name = "firstNameStudent"
+                    placeholder = "First Name"
+                    maxLength = "25"
+                    pattern = "[A-Za-z]{1,25}[A-Za-z]{1,25}?"
+                    title = "Accept Only Letters "
+                    required />
+                    <span className = "focus-input100" />
+                    <span className = "symbol-input100" >
+                    <FontAwesomeIcon icon = { faEnvelopeOpenText }
+                    color = "#1d2a48"
+                    size = "sm" />
+                    </span> 
+                    </div> 
+        
+                    <div className = "wrap-input100 validate-input" >
+                    <input className = "input100"
+                    ref="DateNaissanceEtudiant"
+                    type = "date"
+                    name = "dateNaissanceStudent"
+                    placeholder = "Date de Naissance"
+                    required title = "Give us a valid Date of birth" />
+                    <span className = "focus-input100" />
+                    <span className = "symbol-input100" >
+                    <FontAwesomeIcon icon = { faCalendar }
+                    color = "#1d2a48"
+                    size = "sm" />
+                    </span> 
+                    </div> 
+        
+                    <div className = "wrap-input100 validate-input" >
+                    <input className = "input100"
+                    ref="NiveauEtudiant"
+                    type = "text"
+                    name = "NiveauStudent"
+                    placeholder = "Group"
+                    maxLength = "4"
+                    pattern = "[1-3]CP?S?\d"
+                    title = "Give a valid group. Exemple : 2CP2 / 1CS5 "
+                    required />
+                    <span className = "focus-input100" />
+                    <span className = "symbol-input100" >
+                    <FontAwesomeIcon icon = { faAddressBook }
+                    color = "#1d2a48"
+                    size = "sm" />
+                    </span> 
+                    </div> 
+        
+                    <div className = "wrap-input100 validate-input" >
+                    <input className = "input100"
+                    ref="MatEtudiant"
+                    type = "tel"
+                    name = "matStudent"
+                    placeholder = "Matricule"
+                    required maxLength = "13"
+                    pattern = "[0-9]{13}"
+                    title = "Matricule should contain 13 digit and only digits" />
+                    <span className = "focus-input100" />
+                    <span className = "symbol-input100" >
+                    <FontAwesomeIcon icon = { faBook }
+                    color = "#1d2a48"
+                    size = "sm" />
+                    </span> 
+                    </div> 
+        
+                    <button className = "login100-form-btn"
+                        onClick = {this.ValidateEtudiantClicked } >
+                    Validate 
+                    </button>
+        
+                    </form>
+                </Popover.Content> 
+            </Popover>
+            );
 
             const columns = [{
                 dataField: 'id',

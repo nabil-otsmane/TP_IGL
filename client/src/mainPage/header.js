a97cdee298310278bd404b4c92b349598ab9ff21
nabil-otsmane <hn_otsmane@esi.dz>
2019-12-30 17:40:41 +0100

    feat: added logout func

--- client/src/mainPage/header.js
+++ client/src/mainPage/header.js
@@ -1,39 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCog, faBell, faSignOutAlt, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { withCookies } from 'react-cookie';
+import { useHistory } from 'react-router-dom';
 
+function Header (props) {
 
-class Header extends Component {
-    render() {
+    let history = useHistory();
 
-        return (
-            <nav className="d-flex justify-content-between align-items-center header">
-            <div>GESTION DES {this.props.type.toUpperCase()}</div>
-                <form className="form-inline w-50 justify-content-center">
-                    <div className="input-group w-50">
-                        <input type="search" className="form-control p-1 h-25 text-center" placeholder="Rechercher compte" />
-                        <div className="input-group-append">
-                            <button className="input-group-text btn p-1" type="submit">
-                                <FontAwesomeIcon icon={faSearch} color="#1d2a48" />
-                            </button>
-                        </div>
-                    </div>
-                    
-                </form>
-                <div className="row">
-                    <div className="col">
-                        <FontAwesomeIcon icon={faBell} color="#1d2a48" />
-                    </div>
-                    <div className="col">
-                        <FontAwesomeIcon icon={faUserCog} color="#1d2a48" />
-                    </div>
-                    <div className="col">
-                        <FontAwesomeIcon icon={faSignOutAlt} color="#1d2a48" />
+    function logout() {
+        const {cookies} = props;
+        cookies.remove("jwt_token");
+        history.replace("/login"); 
+    }
+
+    return (
+        <nav className="d-flex justify-content-between align-items-center header">
+        <div>GESTION DES {props.type.toUpperCase()}</div>
+            <form className="form-inline w-50 justify-content-center">
+                <div className="input-group w-50">
+                    <input type="search" className="form-control p-1 h-25 text-center" placeholder="Rechercher compte" />
+                    <div className="input-group-append">
+                        <button className="input-group-text btn p-1" type="submit">
+                            <FontAwesomeIcon icon={faSearch} color="#1d2a48" />
+                        </button>
                     </div>
                 </div>
-            </nav>
-        );
-    }
+                
+            </form>
+            <div className="row">
+                <div className="col">
+                    <button className="btn">
+                        <FontAwesomeIcon icon={faBell} color="#1d2a48" />
+                    </button>
+                </div>
+                <div className="col">
+                    <button className="btn">
+                        <FontAwesomeIcon icon={faUserCog} color="#1d2a48" />
+                    </button>
+                </div>
+                <div className="col">
+                    <button className="btn" onClick={logout}>
+                        <FontAwesomeIcon icon={faSignOutAlt} color="#1d2a48" />
+                    </button>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
-export default Header;
+export default withCookies(Header);
